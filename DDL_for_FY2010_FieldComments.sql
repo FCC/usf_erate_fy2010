bfba6c4fc448801264e2dcf3fb7173409fc54853
Greg Elin <greg@fotonotes.net>
2012-10-11 15:05:20 -0400

    Add FY2010_FieldComments.txt and DDL.

---
+++ DDL_for_FY2010_FieldComments.sql
@@ -0,0 +1,7 @@
+CREATE TABLE FY2010_FieldComments
+(
+  DATA_FILE                   	VARCHAR2(40) NOT NULL,    
+  FIELD        					VARCHAR2(40) NOT NULL,
+  COMMMENT						NUMBER(128) NOT NULL
+)
+
